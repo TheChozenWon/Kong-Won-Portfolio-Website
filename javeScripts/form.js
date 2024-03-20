214a4a998d9ae4dee253ac3b366a27769436863a
Kong Won <won.joseph@yahoo.com>
2024-03-19 22:32:44 -0400

    layout, user interface

---
+++ javeScripts/form.js
@@ -0,0 +1,25 @@
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const yourEmail = document.getElementById('your-email');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+  let messages = [];
+  if (firstName.value === ''  || firstName.value == null) {
+    messages.push('First name is required')
+  }
+
+  if (lastName.value === ''  ||lastName.value == null) {
+    messages.push('Last name is required')
+  }
+
+  if (yourEmail.value === ''  || yourEmail.value == null) {
+    messages.push('Your email is required')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+});
