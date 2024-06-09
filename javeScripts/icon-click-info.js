c054af9f9691cc66771a176c63cf20b938db540b
Kong Won <won.joseph@yahoo.com>
2024-06-08 23:27:00 -0400

    Format fixes

--- javeScripts/icon-click-info.js
+++ javeScripts/icon-click-info.js
@@ -1,51 +1,77 @@
 function myFunction1() {
-  alert('HTML stands for HyperText Markup Language. It is the standard markup language used to create and design documents on the World Wide Web. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document. A markup language consists of a set of markup tags, and HTML uses a system of markup tags to annotate text, images, and other content to be displayed in a web browser.');
+  alert(
+    "HTML stands for HyperText Markup Language. It is the standard markup language used to create and design documents on the World Wide Web. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document. A markup language consists of a set of markup tags, and HTML uses a system of markup tags to annotate text, images, and other content to be displayed in a web browser."
+  );
 }
 
 function myFunction2() {
-  alert('CSS, or Cascading Style Sheets, is a stylesheet language used to describe the presentation of a document written in HTML. It allows web developers to control the layout, appearance, and style of multiple web pages all at once. CSS separates the structure and content of a document from its presentation, providing a way to create consistent and visually appealing designs across a website.');
+  alert(
+    "CSS, or Cascading Style Sheets, is a stylesheet language used to describe the presentation of a document written in HTML. It allows web developers to control the layout, appearance, and style of multiple web pages all at once. CSS separates the structure and content of a document from its presentation, providing a way to create consistent and visually appealing designs across a website."
+  );
 }
 
 function myFunction3() {
-  alert('JavaScript (JS) is a high-level, interpreted programming language primarily known for its role in web development. It enables interactive and dynamic behavior within web browsers, allowing developers to create responsive and feature-rich web applications. JavaScript is an essential component of the modern web stack, working in conjunction with HTML (Hypertext Markup Language) and CSS (Cascading Style Sheets).');
+  alert(
+    "JavaScript (JS) is a high-level, interpreted programming language primarily known for its role in web development. It enables interactive and dynamic behavior within web browsers, allowing developers to create responsive and feature-rich web applications. JavaScript is an essential component of the modern web stack, working in conjunction with HTML (Hypertext Markup Language) and CSS (Cascading Style Sheets)."
+  );
 }
 
 function myFunction4() {
-  alert('Python is a high-level, interpreted programming language known for its readability, simplicity, and versatility. Guido van Rossum created Python and released its first version in 1991. Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming, making it a general-purpose language suitable for a wide range of applications. ');
+  alert(
+    "Python is a high-level, interpreted programming language known for its readability, simplicity, and versatility. Guido van Rossum created Python and released its first version in 1991. Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming, making it a general-purpose language suitable for a wide range of applications. "
+  );
 }
 
 function myFunction5() {
-  alert('Java is a high-level, object-oriented, and platform-independent programming language that was developed by James Gosling and his team at Sun Microsystems in the mid-1990s. It is designed to be versatile, secure, and portable, making it suitable for a wide range of applications, from mobile and web development to large-scale enterprise systems.');
+  alert(
+    "Java is a high-level, object-oriented, and platform-independent programming language that was developed by James Gosling and his team at Sun Microsystems in the mid-1990s. It is designed to be versatile, secure, and portable, making it suitable for a wide range of applications, from mobile and web development to large-scale enterprise systems."
+  );
 }
 
 function myFunction6() {
-  alert('Git is a distributed version control system (VCS) that is widely used for tracking changes in source code during software development. It was created by Linus Torvalds in 2005 and has become the de facto standard for version control in the software development industry. Git is known for its speed, flexibility, and efficiency, allowing multiple developers to work collaboratively on a project.');
+  alert(
+    "Git is a distributed version control system (VCS) that is widely used for tracking changes in source code during software development. It was created by Linus Torvalds in 2005 and has become the de facto standard for version control in the software development industry. Git is known for its speed, flexibility, and efficiency, allowing multiple developers to work collaboratively on a project."
+  );
 }
 
 function myFunction7() {
-  alert('PHP, originally an acronym for "Personal Home Page," now stands for "Hypertext Preprocessor." It is a server-side scripting language designed for web development. PHP is embedded in HTML code and executed on the server, generating dynamic web pages. Developed by Rasmus Lerdorf in 1994, PHP has evolved into a popular and versatile scripting language for creating interactive and dynamic web applications.');
+  alert(
+    'PHP, originally an acronym for "Personal Home Page," now stands for "Hypertext Preprocessor." It is a server-side scripting language designed for web development. PHP is embedded in HTML code and executed on the server, generating dynamic web pages. Developed by Rasmus Lerdorf in 1994, PHP has evolved into a popular and versatile scripting language for creating interactive and dynamic web applications.'
+  );
 }
 
 function myFunction8() {
-  alert('JavaScript (JS) is a high-level, interpreted programming language primarily known for its role in web development. It enables interactive and dynamic behavior within web browsers, allowing developers to create responsive and feature-rich web applications. JavaScript is an essential component of the modern web stack, working in conjunction with HTML (Hypertext Markup Language) and CSS (Cascading Style Sheets).');
+  alert(
+    "JavaScript (JS) is a high-level, interpreted programming language primarily known for its role in web development. It enables interactive and dynamic behavior within web browsers, allowing developers to create responsive and feature-rich web applications. JavaScript is an essential component of the modern web stack, working in conjunction with HTML (Hypertext Markup Language) and CSS (Cascading Style Sheets)."
+  );
 }
 
 function myFunction9() {
-  alert('React, or React.js, is an open-source JavaScript library developed and maintained by Facebook. It is widely used for building user interfaces (UIs) for web applications, with a primary focus on creating reusable UI components. React allows developers to build interactive, dynamic, and efficient user interfaces by efficiently updating and rendering components in response to changes in data.');
+  alert(
+    "React, or React.js, is an open-source JavaScript library developed and maintained by Facebook. It is widely used for building user interfaces (UIs) for web applications, with a primary focus on creating reusable UI components. React allows developers to build interactive, dynamic, and efficient user interfaces by efficiently updating and rendering components in response to changes in data."
+  );
 }
 
 function myFunction10() {
-  alert('TypeScript is a superset of JavaScript, meaning that it builds on and extends JavaScript by adding static typing. It was developed by Microsoft and is an open-source programming language. TypeScript code can be transpiled to plain JavaScript, making it compatible with existing JavaScript codebases and allowing developers to leverage its additional features while working with the JavaScript ecosystem.');
+  alert(
+    "TypeScript is a superset of JavaScript, meaning that it builds on and extends JavaScript by adding static typing. It was developed by Microsoft and is an open-source programming language. TypeScript code can be transpiled to plain JavaScript, making it compatible with existing JavaScript codebases and allowing developers to leverage its additional features while working with the JavaScript ecosystem."
+  );
 }
 
 function myFunction11() {
-  alert('WordPress is a popular open-source content management system (CMS) that allows users to create and manage websites and blogs easily. It is written in PHP and uses a MySQL database to store content and settings. WordPress is known for its user-friendly interface, flexibility, and extensive plugin ecosystem, making it a widely adopted platform for individuals, businesses, and organizations to build and maintain websites without extensive coding knowledge.');
+  alert(
+    "WordPress is a popular open-source content management system (CMS) that allows users to create and manage websites and blogs easily. It is written in PHP and uses a MySQL database to store content and settings. WordPress is known for its user-friendly interface, flexibility, and extensive plugin ecosystem, making it a widely adopted platform for individuals, businesses, and organizations to build and maintain websites without extensive coding knowledge."
+  );
 }
 
 function myFunction12() {
-  alert('Bootstrap is an open-source front-end framework developed by Twitter. It is a powerful and widely used framework for building responsive and mobile-first web applications. Bootstrap provides a set of pre-designed HTML, CSS, and JavaScript components, including typography, forms, buttons, navigation, and other interface elements, making it easier for developers to create consistent and visually appealing web pages.');
+  alert(
+    "Bootstrap is an open-source front-end framework developed by Twitter. It is a powerful and widely used framework for building responsive and mobile-first web applications. Bootstrap provides a set of pre-designed HTML, CSS, and JavaScript components, including typography, forms, buttons, navigation, and other interface elements, making it easier for developers to create consistent and visually appealing web pages."
+  );
 }
 
 function myFunction13() {
-  alert('Node.js is an open-source, cross-platform JavaScript runtime environment that allows developers to execute JavaScript code server-side. It is built on the V8 JavaScript engine, the same engine that powers the Google Chrome browser. Node.js enables the development of scalable and high-performance network applications by using an event-driven, non-blocking I/O model.');
+  alert(
+    "Node.js is an open-source, cross-platform JavaScript runtime environment that allows developers to execute JavaScript code server-side. It is built on the V8 JavaScript engine, the same engine that powers the Google Chrome browser. Node.js enables the development of scalable and high-performance network applications by using an event-driven, non-blocking I/O model."
+  );
 }
